13560cf36afa02c26a89ae3f977b29e7ee48a793
Megha <meghaninave8@gmail.com>
2025-12-25 18:10:22 +0530

    25/12/25

---
+++ movie_management_system/models/movie.js
@@ -0,0 +1,14 @@
+import mongoose, { model } from 'mongoose'
+
+const movies=new mongoose.Schema({
+     Movie_Title:{type:String, required:true},
+     Description:{type:String, required:true},
+     Genre:{type:String, required:true},
+     Genre:{type:String, required:true},
+      Release_Year:{type:Number, required:true},
+       Movie_Poster:{type:String, required:true},
+      
+
+})
+
+export const Movie=mongoose.model("Movie",movies);
